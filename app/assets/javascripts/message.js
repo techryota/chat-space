42daf1b816124b5cc302d0b9dcb3f6e286760c24
techryota <qxnsz54@gmail.com>
2019-10-31 08:56:34 +0900

    automatic-updating

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,25 +1,26 @@
 $(function(){
 
 
-  function buildMessage(message){
-    var html =`<div class="message">
-                <div class="upper-message">
-                  <div class="upper-message__user-name">
-                    ${message.name}
-                  </div>
-                  <div class="upper-message__date">
-                    ${message.data}
-                  </div>
-                </div>
-                <div class="lower-message">
-                  <p class="lower-message__content">
-                    ${message.content}
-                  </p>
-                  <img src="${message.image}">
-                </div>
-               </div>`
+  function buildMessageHTML(message) {
+    var image = (message.image.url) ? `<img src=${message.image.url} class= "lower-message__image" >` : "";
+    var html = `<div class="message" data-id="${message.id}"> 
+          <div class="upper-message">
+            <div class="upper-message__user-name">
+              ${message.user_name}
+            </div>
+            <div class="upper-message__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="lower-meesage">
+            <p class="lower-message__content">
+              ${message.content}
+            </p>
+            ${image}
+          </div>
+        </div>`
     return html;
-  }
+  };
 
   
   $('#new_message').on('submit', function(e){
@@ -35,15 +36,40 @@ $(function(){
       contentType: false
     })
     .done(function(message){
-      var html = buildMessage(message);
+      var html = buildMessageHTML(message);
       $('.messages').append(html);
-      $('#message_content')[0].reset();
+      $('#new_message')[0].reset();
       $(".form__submit").removeAttr("disabled");
-      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight });
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight }, 'fast');
     })
     .fail(function(){
       alert('エラー');
     })
-    
   })
+
+  
+  function reloadMessages() {
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){
+      var last_message_id = $('.message').last().data("id");
+      
+      $.ajax({
+        url: 'api/messages',
+        type: 'GET',
+        dataType: 'json',
+        data: { id: last_message_id }
+      })
+      .done(function (messages) {
+        var insertHTML = '';
+        messages.forEach(function (message) {  
+          insertHTML = buildMessageHTML(message);
+          $('.messages').append(insertHTML);
+        })
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight }, 'fast');
+      })
+      .fail(function () {
+        alert('エラー');
+      });
+    }
+  };
+  setInterval(reloadMessages, 5000);
 });
